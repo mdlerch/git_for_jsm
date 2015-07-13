9ea02d4d16d4de8c018b6faeeec5bab20a23ee62
Michael Lerch <mdlerch@gmail.com>
2015-07-13 12:52:56 -0600

    Add example "bad" file management

---
+++ demo/my_research/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+touch analysis_script.R --date "2015-04-05 09:14"
+touch analysis_script_1.R --date "2015-04-06 11:49"
+touch analysis_script_06_09_15.R --date "2015-6-09 14:31"
+touch analysis_script_06_12_15.R --date "2015-6-12 10:57"
+touch analysis_script_final_06_19_15.R --date "2015-06-12 22:19"
+touch analysis_script_06_24_15.R --date "2015-07-02 17:06"
+touch analysis_script_a.R --date "2015-05-05 08:03"
+
+touch manuscript.docx --date "2015-05-11 11:46"
+touch manuscript_2.docx --date "2015-07-03 16:26"
+touch manuscript_2_personA_edits.docx --date "2015-08-01 12:14"
+touch manuscript_07_03_15.docx --date "2015-07-03 16:27"
+touch manuscript_draft.docx --date "2015-05-12 08:36"
+touch manuscript_final.docx --date "2015-06-29 20:14"
